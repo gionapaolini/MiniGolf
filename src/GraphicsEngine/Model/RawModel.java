5f389319f5845062ff2d3229d1d3653fabbfe600
gionapaolini <gionapaolini@gmail.com>
2016-06-04 20:22:51 +0200

    Add Loader, RawModel and Renderer classes

---
+++ src/GraphicsEngine/Model/RawModel.java
@@ -0,0 +1,23 @@
+package GraphicsEngine.Model;
+
+/**
+ * Created by giogio on 04/06/16.
+ */
+public class RawModel {
+
+    private int vaoID;
+    private int vertexCount;
+
+    public RawModel(int vaoID, int vertexCount) {
+        this.vaoID = vaoID;
+        this.vertexCount = vertexCount;
+    }
+
+    public int getVaoID() {
+        return vaoID;
+    }
+
+    public int getVertexCount() {
+        return vertexCount;
+    }
+}
--- src/Tests/Test.java
+++ src/Tests/Test.java
@@ -1,6 +1,9 @@
 package Tests;
 
 import GraphicsEngine.DisplayManager;
+import GraphicsEngine.Model.Loader;
+import GraphicsEngine.Model.RawModel;
+import GraphicsEngine.Model.Renderer;
 import org.lwjgl.opengl.Display;
 
 /**
@@ -12,10 +15,19 @@ public class Test {
 
         DisplayManager.createDisplay("CrazyGolf Game");
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = { -0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f, -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
         while (!Display.isCloseRequested()){
+            renderer.prepare();
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
-
+        loader.cleanUP();
         DisplayManager.closeDisplay();
 
 
