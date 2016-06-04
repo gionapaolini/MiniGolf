4f49b14b2e13e9a147c8910e975249450f571022
gionapaolini <gionapaolini@gmail.com>
2016-06-04 19:55:04 +0200

    Add DisplayManager class

--- src/Tests/Test.java
+++ src/Tests/Test.java
@@ -1,8 +1,24 @@
 package Tests;
 
+import GraphicsEngine.DisplayManager;
+import org.lwjgl.opengl.Display;
+
 /**
  * Created by giogio on 04/06/16.
  */
 public class Test {
+
+    public static void main(String[] args){
+
+        DisplayManager.createDisplay("CrazyGolf Game");
+
+        while (!Display.isCloseRequested()){
+            DisplayManager.updateDisplay();
+        }
+
+        DisplayManager.closeDisplay();
+
+
+    }
 
 }
