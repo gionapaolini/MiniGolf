547bfa33f02c90c79ce79684be7f93bab06f2e2e
gionapaolini <gionapaolini@gmail.com>
2016-06-05 17:16:48 +0200

    Add unifor variable loader

--- src/GraphicsEngine/Shaders/StaticShader.java
+++ src/GraphicsEngine/Shaders/StaticShader.java
@@ -1,11 +1,15 @@
 package GraphicsEngine.Shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 /**
  * Created by giogio on 05/06/16.
  */
 public class StaticShader extends ShaderProgram {
     private static final String VERTEX_FILE = "src/GraphicsEngine/Shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/GraphicsEngine/Shaders/fragmentShader.txt";
+
+    private int location_transformationMatrix;
 
     public StaticShader(){
         super(VERTEX_FILE,FRAGMENT_FILE);
@@ -15,5 +19,14 @@ public class StaticShader extends ShaderProgram {
     protected void bindAttributes(){
         super.bindAttributes(0,"positions");
         super.bindAttributes(1,"textureCoords");
+    }
+
+    @Override
+    protected void getAllUniformLocations(){
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+
+    public void loadTransformationMatrix(Matrix4f matrix){
+        super.loadMatrix(location_transformationMatrix,matrix);
     }
 }
