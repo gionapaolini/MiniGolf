1c5c97caa6afab051b706fea9d2ddf44b52e0d2d
gionapaolini <gionapaolini@gmail.com>
2016-06-06 15:25:10 +0200

    Add OBJLoader class

--- src/Tests/Test.java
+++ src/Tests/Test.java
@@ -5,6 +5,7 @@ import GraphicsEngine.Entities.Entity;
 import GraphicsEngine.RenderEngine.DisplayManager;
 import GraphicsEngine.RenderEngine.Loader;
 import GraphicsEngine.Model.RawModel;
+import GraphicsEngine.RenderEngine.OBJLoader;
 import GraphicsEngine.RenderEngine.Renderer;
 import GraphicsEngine.Model.TexturedModel;
 import GraphicsEngine.Shaders.StaticShader;
@@ -26,87 +27,9 @@ public class Test {
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
 
-        float[] vertices = {
-                -0.5f,0.5f,0,
-                -0.5f,-0.5f,0,
-                0.5f,-0.5f,0,
-                0.5f,0.5f,0,
-
-                -0.5f,0.5f,1,
-                -0.5f,-0.5f,1,
-                0.5f,-0.5f,1,
-                0.5f,0.5f,1,
-
-                0.5f,0.5f,0,
-                0.5f,-0.5f,0,
-                0.5f,-0.5f,1,
-                0.5f,0.5f,1,
-
-                -0.5f,0.5f,0,
-                -0.5f,-0.5f,0,
-                -0.5f,-0.5f,1,
-                -0.5f,0.5f,1,
-
-                -0.5f,0.5f,1,
-                -0.5f,0.5f,0,
-                0.5f,0.5f,0,
-                0.5f,0.5f,1,
-
-                -0.5f,-0.5f,1,
-                -0.5f,-0.5f,0,
-                0.5f,-0.5f,0,
-                0.5f,-0.5f,1
-
-        };
-
-        float[] textureCoords = {
-
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-
-
-        };
-
-        int[] indices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-
-        };
         Camera camera = new Camera();
 
-        RawModel model = loader.loadToVAO(vertices, textureCoords,indices);
+        RawModel model = OBJLoader.loadObjModel("tree", loader);
         ModelTexture texture1 = new ModelTexture(loader.loadTexture("tree"));
         TexturedModel texturedModel = new TexturedModel(model,texture1);
         Entity entity = new Entity(texturedModel,new Vector3f(0,0,-5),0,0,0,1);
