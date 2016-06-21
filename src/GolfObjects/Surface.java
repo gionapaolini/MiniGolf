6ecae94cd2e16c9efd50564dd559d144ca428905
gionapaolini <gionapaolini@gmail.com>
2016-06-22 00:11:20 +0200

    Now friction works properly

--- src/GolfObjects/Surface.java
+++ src/GolfObjects/Surface.java
@@ -29,26 +29,38 @@ public class Surface extends GolfObject{
         return coefficientFriction;
     }
     public boolean ballIsOver(GolfObject obj){
-        float as_x = obj.getPosition().x-points[0].x;
-        float as_y = obj.getPosition().z-points[0].z;
+        Vector3f[] pont = new Vector3f[3];
 
-        boolean s_ab = (points[1].x-points[0].x)*as_y-(points[1].z-points[0].z)*as_x > 0;
+        for (int i=0;i<3;i++){
+            pont[i] = new Vector3f(points[i].x+getModel().getPosition().x,0,points[i].z+getModel().getPosition().z);
+        }
+        float as_x = obj.getPosition().x-pont[0].x;
+        float as_y = obj.getPosition().z-pont[0].z;
 
-        if((points[2].x-points[0].x)*as_y-(points[2].z-points[0].z)*as_x > 0 == s_ab) return false;
 
-        if((points[2].x-points[1].x)*(obj.getPosition().z-points[1].z)-(points[2].z-points[1].z)*(obj.getPosition().x-points[1].x) > 0 != s_ab) return false;
+        boolean s_ab = (pont[1].x-pont[0].x)*as_y-(pont[1].z-pont[0].z)*as_x > 0;
+
+        if((pont[2].x-pont[0].x)*as_y-(pont[2].z-pont[0].z)*as_x > 0 == s_ab) return false;
+
+        if((pont[2].x-pont[1].x)*(obj.getPosition().z-pont[1].z)-(pont[2].z-pont[1].z)*(obj.getPosition().x-pont[1].x) > 0 != s_ab) return false;
 
         return true;
     }
     public boolean mouseOver(Vector3f point){
-        float as_x = point.x-points[0].x;
-        float as_y = point.z-points[0].z;
+        Vector3f[] pont = new Vector3f[3];
 
-        boolean s_ab = (points[1].x-points[0].x)*as_y-(points[1].z-points[0].z)*as_x > 0;
+        for (int i=0;i<3;i++){
+            pont[i] = new Vector3f(points[i].x+getModel().getPosition().x,0,points[i].z+getModel().getPosition().z);
+        }
+        float as_x = point.x-pont[0].x;
+        float as_y = point.z-pont[0].z;
 
-        if((points[2].x-points[0].x)*as_y-(points[2].z-points[0].z)*as_x > 0 == s_ab) return false;
 
-        if((points[2].x-points[1].x)*(point.z-points[1].z)-(points[2].z-points[1].z)*(point.x-points[1].x) > 0 != s_ab) return false;
+        boolean s_ab = (pont[1].x-pont[0].x)*as_y-(pont[1].z-pont[0].z)*as_x > 0;
+
+        if((pont[2].x-pont[0].x)*as_y-(pont[2].z-pont[0].z)*as_x > 0 == s_ab) return false;
+
+        if((pont[2].x-pont[1].x)*(point.z-pont[1].z)-(pont[2].z-pont[1].z)*(point.x-pont[1].x) > 0 != s_ab) return false;
 
         return true;
     }
